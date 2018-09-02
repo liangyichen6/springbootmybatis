00d6e57552d072b0e29d43cf26f6a62d8aa130de
陈良毅 <cclchen1992@163.com>
2018-09-02 14:56:53 +0800

    1.Practice Mybatis Generate plugin
    1. mybatis gernerate plugin 通过pluginmanagement 增加
    2. remove comment generate by plugin
    3. enable override

---
+++ src/main/java/ivan/learn/springboot/mybatis/dao/CustomerMapper.java
@@ -0,0 +1,18 @@
+package ivan.learn.springboot.mybatis.dao;
+
+import ivan.learn.springboot.mybatis.model.Customer;
+
+public interface CustomerMapper {
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Customer record);
+
+    int insertSelective(Customer record);
+
+    Customer selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(Customer record);
+
+    int updateByPrimaryKey(Customer record);
+}
---
+++ src/main/java/ivan/learn/springboot/mybatis/model/Customer.java
@@ -0,0 +1,26 @@
+package ivan.learn.springboot.mybatis.model;
+
+import java.util.Date;
+
+
+public class Customer {
+
+    private Long id;
+
+    private String name;
+
+    private Boolean gender;
+
+    private Integer age;
+
+    private String phone;
+
+    private String firstName;
+
+    private String lastName;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+}
